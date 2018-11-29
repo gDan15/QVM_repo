a8046a331971212f0518bad05c7f342e715d00f5
gDan15 <gaetan.guru@gmail.com>
2018-11-29 17:48:02 +0100

    Advancing in code.

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf600
-{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-}
 # QVM - Python
 ## Version
 - #### Python 3.7.1
-## Packages installés
+## Installed packages
 - #### pip install pyquil
+## Use
+### Start servers like so:
+- #### $ qvm -S
+- #### $ quilc -S
--- software_building.py
+++ software_building.py
@@ -4,17 +4,18 @@ from pyquil import get_qc
 
 
 p = Program()
-# Declaration of a memory space where measurements will be read. The memory is equal to 1 bit.
+# Declaration of a memory space where measurements will be read. The memory is equal to 1 bit in this case.
 ro = p.declare('ro','BIT', 1)
 # La porte quantique X est appliquée au qubit 0
 p += X(0)
-p += MEASURE(0, ro[0])C
+p += MEASURE(0, ro[0])
 # La représentation du programme en Quil est affichée lorsque p est imprimé.
 # print(p)
 
 # qc = simulation of a quantic computer
 qc = get_qc('1q-qvm')  # You can make any 'nq-qvm' this way for any reasonable 'n'
 executable = qc.compile(p)
+# L'instruction ci-dessous permet d'exécuter une instruction de la même manière que sur un QPU.
 result = qc.run(executable)
 print(result)
 
